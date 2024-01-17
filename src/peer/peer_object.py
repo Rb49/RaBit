4e38b86fd9ded3a682ff73f93b62c7627ba97faa
Roey Borovsky <roeyboro@gmail.com>
2024-01-17 09:47:24 +0200

    added peer object

---
+++ src/peer/peer_object.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass
+from typing import Optional, List, Union, Tuple
+
+
+@dataclass
+class PeerObject(object):
+    """
+    object to store attributes of a peer and some stats
+    """
+
+    address: Tuple[str, int]  # the address of this peer (ip, port)
+    # status of this peer
+    isChocked: bool  # is the peer chocked rn?
+    isInterested: bool  # is the peer interested rn?
+
+    # some stats
+    client: Optional[Union[str, None]] = None  # which client is this peer using?
+    downloaded: Optional[int] = 0  # how much data has been downloaded from this peer
+    uploaded: Optional[int] = 0  # how much data has been uploaded to this peer
+    flags: Optional[List[str]] = None  # flags - other information over the status of this peer
