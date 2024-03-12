306a270da533596f054178c5501dc0211f69603a
Roey Borovsky <roeyboro@gmail.com>
2024-03-13 00:53:56 +0200

    test file, ignore

--- src/client/test.py
+++ src/client/test.py
@@ -1,34 +1,7 @@
-import numpy as np
-import queue
+A = {2, 3}
+B = {2, 3, 4}
+A.add(5)
+# Elements in B that are not in A
+elements_in_B_not_in_A = B - A
 
-
-def floodfillNd(board: np.ndarray, background: int, color: int, *seed) -> None:
-    directions = []
-    seed = list(seed)
-    for i in range(len(seed)):
-        seedCopy = seed.copy()
-        seedCopy[i] += 1
-        directions.append(tuple(seedCopy))
-        seedCopy = seed.copy()
-        seedCopy[i] -= 1
-        directions.append(tuple(seedCopy))
-
-    for coords in directions:
-        try:
-            if board[coords] == background:
-                board[coords] = color
-                floodfillNd(board, background, color, *coords)
-        except IndexError:
-            continue
-
-
-if __name__ == '__main__':
-    arr = np.zeros((4, 4, 4, 4))
-    arr[1:3, 1:3, 1:3, 1:3] = 1
-    start = (0, 0, 0, 0)
-
-    print('before:\n', arr)
-
-    floodfillNd(arr, 0, 2, *start)
-
-    print('after:\n', arr)
+print(elements_in_B_not_in_A)
