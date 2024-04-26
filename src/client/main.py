20f179d9ab14ab6adeeacbd8d6506b232632a9b4
Roey Borovsky <roeyboro@gmail.com>
2024-04-26 21:42:34 +0300

    downloading multiple torrents at the same time

--- src/client/main.py
+++ src/client/main.py
@@ -1,9 +1,38 @@
+import src.app_data.db_utils as db_utils
 from src.seeding.server import start_seeding_server
 from src.download.download_session_object import DownloadSession
 
 import threading
 import asyncio
 import time
+
+
+def main(*torrents, **kwargs):
+    seeding_thread = threading.Thread(target=lambda: asyncio.run(start_seeding_server()), daemon=True)
+    seeding_thread.start()
+
+    # TODO wait for the seeding server before starting download
+    while True:
+        from src.seeding.server import SEEDING_SERVER_IS_UP
+        if SEEDING_SERVER_IS_UP:
+            break
+        time.sleep(0.5)
+
+    download_dir = kwargs.get('download_dir')
+    if not download_dir:
+        download_dir = db_utils.get_configuration('download_dir')
+
+    threads = []
+    for torrent_path in torrents:
+        session = DownloadSession(torrent_path, download_dir)
+        download_thread = threading.Thread(target=lambda: asyncio.run(session.download()), daemon=True)
+        threads.append(download_thread)
+        download_thread.start()
+    for thread in threads:
+        thread.join()
+        print('download complete!')
+
+    seeding_thread.join()
 
 
 # TODO organize all files, add error messages with exceptions, documentation, type hints, ...
@@ -15,31 +44,15 @@ if __name__ == '__main__':
     if sys.version_info[0:2] != (3, 10):
         raise Exception("Wrong Python version! Use version 3.10 only.")
 
-    seeding_thread = threading.Thread(target=lambda: asyncio.run(start_seeding_server()), daemon=True)
-    seeding_thread.start()
+    # torrent_path = r"C:\Users\roeyb\OneDrive\Documents\GitHub\RaBit\RaBit\data\ubuntu-23.10-live-server-amd64.iso.torrent"
+    # torrent_path = r"C:\Users\roeyb\OneDrive\Documents\GitHub\RaBit\RaBit\data\debian-edu-12.4.0-amd64-netinst.iso.torrent"
+    # torrent_path = r"C:\Users\roeyb\OneDrive\Documents\GitHub\RaBit\RaBit\data\The.Rock.1996.1080p.BluRay.x265-RARBG.torrent"
 
-    # TODO wait for the seeding server before starting download
-    while True:
-        from src.seeding.server import SEEDING_SERVER_IS_UP
-        if SEEDING_SERVER_IS_UP:
-            break
-        time.sleep(0.5)
-
-    # torrent_path = r"C:\Users\roeyb\OneDrive\Documents\GitHub\RaBit\RaBit\data\ubuntu-23.10-live-server-amd64.iso.TorrentData"
-    # torrent_path = r"C:\Users\roeyb\OneDrive\Documents\GitHub\RaBit\RaBit\data\debian-edu-12.4.0-amd64-netinst.iso.TorrentData"
-    # torrent_path = r"C:\Users\roeyb\OneDrive\Documents\GitHub\RaBit\RaBit\data\The Best American Short Stories, 2011–2023 (13 books).TorrentData"
-    # torrent_path = r"C:\Users\roeyb\OneDrive\Documents\GitHub\RaBit\RaBit\data\Young.Sheldon.S07E01.HDTV.x264-TORRENTGALAXY.TorrentData"
-    torrent_path = r"C:\Users\roeyb\OneDrive\Documents\GitHub\RaBit\RaBit\data\The Complete Art of War_ Sun Tzu-Sun Pin [blackatk].torrent"
-    # torrent_path = r"C:\Users\roeyb\OneDrive\Documents\GitHub\RaBit\RaBit\data\The.Rock.1996.1080p.BluRay.x265-RARBG.TorrentData"
-
-    result_path = r"C:\Users\roeyb\OneDrive\Documents\GitHub\RaBit\RaBit\results"
-
-    session = DownloadSession(torrent_path, result_path)
-    download_thread = threading.Thread(target=lambda: asyncio.run(session.download()), daemon=True)
-    download_thread.start()
-
-    download_thread.join()
-    print('download complete!')
-    seeding_thread.join()
+    main(
+        r"C:\Users\roeyb\OneDrive\Documents\GitHub\RaBit\RaBit\data\The Best American Short Stories, 2011–2023 (13 books).torrent",
+        r"C:\Users\roeyb\OneDrive\Documents\GitHub\RaBit\RaBit\data\Young.Sheldon.S07E01.HDTV.x264-TORRENTGALAXY.torrent",
+        r"C:\Users\roeyb\OneDrive\Documents\GitHub\RaBit\RaBit\data\The Complete Art of War_ Sun Tzu-Sun Pin [blackatk].torrent",
+        result_path=r"C:\Users\roeyb\OneDrive\Documents\GitHub\RaBit\RaBit\results"
+    )
 
     exit(0)
