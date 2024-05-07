5b8051f1160f58ad8a856a03c946d2b0a66a430f
Roey Borovsky <roeyboro@gmail.com>
2024-05-08 00:53:57 +0300

    torrents' progress table now works

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 from src.RaBit import Client
 from src.view.loading_window import get_LoadingWindow
 from src.view.main_window import get_MainWindow
+from src.view.utils import *
 
 import threading
+import asyncio
 import time
 
 
@@ -13,15 +15,54 @@ def wait_for_loading(client: Client, window: get_LoadingWindow()):
     window.destroy()
 
 
-def gui_updates(client: Client, main_window: get_MainWindow()):
+async def gui_updates(client: Client, main_window: get_MainWindow()):
     first = dict()
     while True:
-        for torrent in client.torrents:
+        for torrent in client.torrents.copy():
             if torrent not in first:
                 first[torrent] = True
-            main_window.torrents_info_frame.add_torrent(first[torrent], torrent.name, torrent.length, torrent.progress, torrent.state, len(torrent.peers), torrent.ETA)
+            # update progress info
+            main_window.torrents_info_frame.add_torrent(first[torrent], hash(torrent),
+                                                        torrent.name,
+                                                        torrent.length,
+                                                        torrent.progress,
+                                                        torrent.state,
+                                                        len(torrent.peers),
+                                                        torrent.ETA)
             first[torrent] = False
-        time.sleep(0.5)
+
+            if torrent.TorrentData if hasattr(torrent, "TorrentData") else True:
+                # update initialized objects only
+                if main_window.current_obj_hash == hash(torrent):
+                    # update displayed window only
+                    if main_window.current_tab.get() == "General":
+                        # update general info
+                        main_window.update_general_info_tab(hash(torrent),
+                                                            convert_size(torrent.downloaded),
+                                                            convert_size(torrent.uploaded),
+                                                            convert_size(torrent.corrupted),
+                                                            convert_size(torrent.wasted),
+                                                            f"{torrent.progress}%",
+                                                            round(torrent.uploaded / (torrent.downloaded + torrent.wasted + torrent.corrupted + 1), 2),
+                                                            f"{(num := torrent.num_pieces if hasattr(torrent, 'num_pieces') else len(torrent.TorrentData.piece_hashes))} (have {round(num * torrent.progress / 100)})",
+                                                            convert_size(torrent.piece_length if hasattr(torrent, "piece_length") else torrent.TorrentData.info[b'piece length']),
+                                                            len(list(filter(lambda x: x.state, torrent.trackers))),
+                                                            torrent.TorrentData.comment if hasattr(torrent, "TorrentData") else torrent.comment,
+                                                            torrent.TorrentData.created_by if hasattr(torrent, "TorrentData") else torrent.created_by,
+                                                            torrent.TorrentData.date_created if hasattr(torrent, "TorrentData") else torrent.date_created)
+                    elif main_window.current_tab.get() == "Peers":
+                        # update peers info
+                        ...
+
+        prev_torrents = client.torrents.copy()
+        await client.torrents_state_update_loop()
+
+        # handle removed torrents
+        removed = prev_torrents - client.torrents
+        if removed:
+            main_window.torrents_info_frame.remove_torrent([hash(torrent) for torrent in removed])
+        else:
+            await asyncio.sleep(0.5)
 
 
 # start the client and display the loading window
@@ -31,5 +72,5 @@ wait_for_loading(myClient, loading_window)
 
 # start the main window
 main_window = get_MainWindow()()
-threading.Thread(target=lambda: gui_updates(myClient, main_window), daemon=True).start()
+threading.Thread(target=lambda: asyncio.run(gui_updates(myClient, main_window)), daemon=True).start()
 main_window.mainloop()
--- src/view/main_window.py
+++ src/view/main_window.py
@@ -1,9 +1,10 @@
-import tkinter
 from pathlib import Path
-from typing import List, Dict
+from typing import List, Any
 import customtkinter
 from PIL import Image
+from random import choice
 
+from src.RaBit import Client
 from .add_torrent import get_TopWindow
 from .zoomable_canvas import get_ZoomableMapCanvas
 from .utils import *
@@ -35,11 +36,12 @@ class ToolbarFrame(customtkinter.CTkFrame):
 
 
 class TorrentsInfo(customtkinter.CTkScrollableFrame):
-    torrent_labels: List[List] = []
+    torrent_labels: List[List[Any]] = []
 
     def __init__(self, master, **kwargs):
         super().__init__(master, **kwargs)
         self.master = master
+        self.current_row = 0
         columns_titles = [('Select', 0), ('Name', 5), ('Size', 2), ('Progress', 6), ('Status', 2), ('Peers', 1), ('ETA', 2)]
         for i, data in enumerate(columns_titles):
             title, weight = data
@@ -47,14 +49,14 @@ class TorrentsInfo(customtkinter.CTkScrollableFrame):
             self.columnconfigure(i, weight=weight)
             label.grid(row=0, rowspan=1, column=i, pady=(0, 2), sticky="ew")
 
-    def add_torrent(self, first_add: bool, name: str, size: int, progress: float, status: str, peers: int, ETA: float):
+    def add_torrent(self, first_add: bool, obj_hash: int, name: str, size: int, progress: float, status: str, peers: int, ETA: float):
         if first_add:
             labels = [0] * 7
-            row = len(TorrentsInfo.torrent_labels) + 1
+            self.current_row += 1
             for i, param in enumerate([None, name, size, progress, status, peers, ETA]):
                 if i == 0:  # radio button select
-                    labels[i] = customtkinter.CTkRadioButton(self, text=f"No. {row}",
-                                                             variable=self.master.selected_row, value=row,
+                    labels[i] = customtkinter.CTkRadioButton(self, text=f"No. {self.current_row}",
+                                                             variable=self.master.selected_hash, value=obj_hash,
                                                              command=self.master.open_torrent_info_tab)
                 elif i == 1:  # name
                     labels[i] = customtkinter.CTkTextbox(self, fg_color="transparent", activate_scrollbars=True, wrap="none", font=("", 13), height=15)
@@ -71,33 +73,56 @@ class TorrentsInfo(customtkinter.CTkScrollableFrame):
                 else:  # status, peers
                     labels[i] = customtkinter.CTkLabel(self, text=str(param), font=("", 13), justify="left")
 
-                labels[i].grid(row=row, rowspan=1, column=i, pady=(0, 2), padx=4, sticky="ew")
+                labels[i].grid(row=self.current_row, rowspan=1, column=i, pady=(0, 2), padx=4, sticky="ew")
 
+            labels.append(obj_hash)
+            labels.append(self.current_row)
             TorrentsInfo.torrent_labels.append(labels)
             labels[0].invoke()  # display current torrent stats
+
         else:  # update
-            for row in range(1, len(TorrentsInfo.torrent_labels) + 1):
-                if self.grid_slaves(row=row, column=1)[0].get("0.0", "end").strip('\n') == name:
+            for row in TorrentsInfo.torrent_labels:
+                if row[7] == obj_hash:
                     # progress
-                    self.grid_slaves(row=row, column=3)[0].set(progress / 100)
+                    self.grid_slaves(row=row[8], column=3)[0].set(progress / 100)
                     # else
-                    self.grid_slaves(row=row, column=4)[0].configure(text=str(status))
-                    self.grid_slaves(row=row, column=5)[0].configure(text=str(peers))
-                    self.grid_slaves(row=row, column=6)[0].configure(text=convert_seconds(ETA))
+                    self.grid_slaves(row=row[8], column=4)[0].configure(text=str(status))
+                    self.grid_slaves(row=row[8], column=5)[0].configure(text=str(peers))
+                    self.grid_slaves(row=row[8], column=6)[0].configure(text=convert_seconds(ETA))
                     return
 
+    def remove_torrent(self, obj_hashes: List[int]):
+        for rowno, row in reversed(list(enumerate(TorrentsInfo.torrent_labels))):
+            if row[7] in obj_hashes:
+                for item in row[:7]:
+                    item.destroy()
+                TorrentsInfo.torrent_labels.pop(rowno)
+                tab = self.master.info_tabs.pop(row[7])
+                self.master.open_torrent_info_tab(True)
+                tab.destroy()
 
 
 class GeneralTorrentInfo(customtkinter.CTkFrame):
-    def __init__(self, master, **kwargs):
+    titles = [
+        "Downloaded: ", "Uploaded: ", "Corrupted: ", "Wasted: ",
+        "Progress: ", "Ratio: ", "Pieces: ", "Piece length: ",
+        "Trackers: ", "Comment: ", "Created By: ", "Creation Date: "
+    ]
+
+    def __init__(self, master, *data, **kwargs):
+        """
+        titles in order:
+        "Downloaded: ", "Uploaded: ", "Corrupted: ", "Wasted: ",
+        "Progress: ", "Ratio: ", "Pieces: ", "Piece length: ",
+        "Trackers: ", "Comment: ", "Created By: ", "Creation Date: "
+        """
         super().__init__(master, **kwargs)
         master.columnconfigure(0, weight=1)
         master.rowconfigure(0, weight=1)
-        torrent_info = [
-            "Downloaded: ", "Uploaded: ", "Corrupted: ", "Wasted: ",
-            "Progress: ", "Ratio: ", "Pieces: ", "Piece length: ",
-            "Multi file: ", "Comment: ", "Created By: ", "Creation Date: "
-        ]
+        if data:
+            torrent_info = list(map(lambda x: x[0] + str(x[1]), zip(GeneralTorrentInfo.titles, data)))
+        else:
+            torrent_info = GeneralTorrentInfo.titles
         self.columnconfigure(0, weight=1)
         self.columnconfigure(1, weight=1)
         self.columnconfigure(2, weight=1)
@@ -130,25 +155,7 @@ class PeersInfoFrame(customtkinter.CTkScrollableFrame):
             label.grid(row=self.current_row, rowspan=1, column=i, pady=(0, 2), sticky="ew")
 
     def new_peers_info(self, info_hash: bytes, peers):
-        self.current_row += 1
-        labels = [0] * 6
-        for i, param in enumerate([name, size, progress, status, peers, availability]):
-            if i not in (0, 1, 2):  # not progress and name
-                labels[i] = customtkinter.CTkLabel(self, text=str(param), font=("", 13), justify="left")
-            elif i == 1:  # size
-                labels[i] = customtkinter.CTkLabel(self, text=convert_size(param), font=("", 13), justify="left")
-            elif i == 2:  # progress
-                labels[i] = customtkinter.CTkProgressBar(self, orientation="horizontal", mode="determinate",
-                                                         width=50, corner_radius=0, progress_color="green")
-                labels[i].set(progress / 100)
-            else:  # name
-                labels[i] = customtkinter.CTkTextbox(self, fg_color="transparent", activate_scrollbars=True, wrap="none", font=("", 13), height=15)
-                labels[i].insert("0.0", name)
-                labels[i].configure(state="disabled")
-
-            labels[i].grid(row=self.current_row, rowspan=1, column=i, pady=(0, 2), sticky="ew")
-
-        TorrentsInfo.torrent_labels[info_hash] = labels
+        ...
 
     def remove_torrent(self, info_hash: bytes):
         ...
@@ -181,53 +188,63 @@ class MainWindow(customtkinter.CTk):
         self.toolbar_frame.grid(row=0, column=0, padx=10, pady=(3, 0), columnspan=1, rowspan=1, sticky="w")
         self.toplevel_window = None
 
-        self.selected_row = customtkinter.IntVar(value=0)
+        self.selected_hash = customtkinter.IntVar(value=0)
 
         # torrent info holder
         self.torrents_info_frame = TorrentsInfo(self, orientation="vertical")
-        self.torrents_info_frame.grid(row=1, column=0, rowspan=3, columnspan=2, padx=10, pady=(5, 0), sticky="news")
+        self.torrents_info_frame.grid(row=1, column=0, rowspan=3, columnspan=2, padx=10, pady=5, sticky="news")
 
         self.addresses = []
 
         self.info_tabs = dict()
         self.current_tab = None
-
-
+        self.current_obj_hash = None
 
     def open_toplevel(self):
         if self.toplevel_window is None or not self.toplevel_window.winfo_exists():
-            self.toplevel_window = get_TopWindow()(self, "")
+            self.toplevel_window = get_TopWindow()(self, Client().get_download_dir())
         else:
             self.toplevel_window.focus()
 
-    def open_torrent_info_tab(self):
-        row = self.selected_row.get()
-        # get name
-        name = self.torrents_info_frame.grid_slaves(row=row, column=1)[0].get("0.0", "end").strip('\n')
-        if name not in self.info_tabs:
-            self.info_tabs[name] = customtkinter.CTkTabview(master=self)
+    def update_general_info_tab(self, obj_hash: int, *new_data):
+        slaves = reversed(self.info_tabs[obj_hash].tab("General").grid_slaves()[0].grid_slaves())
+        for index, pair in enumerate(zip(slaves, new_data)):
+            slave, info = pair
+            slave.configure(text=GeneralTorrentInfo.titles[index] + str(info))
 
-            self.info_tabs[name].add("General")
-            self.info_tabs[name].add("Peers")
-            self.info_tabs[name].set("General")
-            self.info_tabs[name].tab("Peers").columnconfigure(0, weight=10)
-            self.info_tabs[name].tab("Peers").columnconfigure(1, weight=1)
+    def open_torrent_info_tab(self, random_choice: bool = False):
+        if random_choice:
+            obj_hash = choice(list(self.info_tabs))
+            self.selected_hash.set(obj_hash)
+
+        obj_hash = self.selected_hash.get()
+
+        if obj_hash not in self.info_tabs:
+            self.info_tabs[obj_hash] = customtkinter.CTkTabview(master=self)
+
+            self.info_tabs[obj_hash].add("General")
+            self.info_tabs[obj_hash].add("Peers")
+            self.info_tabs[obj_hash].set("General")
+            self.info_tabs[obj_hash].tab("Peers").columnconfigure(0, weight=10)
+            self.info_tabs[obj_hash].tab("Peers").columnconfigure(1, weight=1)
 
             # peer info and map holder
-            map_frame = MapFrame(self.info_tabs[name].tab("Peers"), self.addresses, fg_color="transparent")
+            map_frame = MapFrame(self.info_tabs[obj_hash].tab("Peers"), self.addresses, fg_color="transparent")
             map_frame.grid(row=0, column=1, columnspan=1, padx=(5, 0), pady=0, sticky="e")
-            peers_info_frame = PeersInfoFrame(self.info_tabs[name].tab("Peers"))
+            peers_info_frame = PeersInfoFrame(self.info_tabs[obj_hash].tab("Peers"))
             peers_info_frame.grid(row=0, column=0, columnspan=1, padx=(0, 5), pady=0, sticky="we")
 
             # general info
-            general_info_tab = GeneralTorrentInfo(self.info_tabs[name].tab("General"), fg_color="transparent")
+            general_info_tab = GeneralTorrentInfo(self.info_tabs[obj_hash].tab("General"), fg_color="transparent")
             general_info_tab.grid(row=0, column=0, padx=5, pady=5, sticky="new")
 
         if self.current_tab:
+            self.current_tab.tab("Peers").grid_slaves(row=0, column=1)[0].map.show_image(True)
             self.current_tab.grid_forget()
 
-        self.info_tabs[name].grid(row=4, column=0, rowspan=10, columnspan=2, padx=10, pady=(0, 5), sticky="news")
-        self.current_tab = self.info_tabs[name]
+        self.info_tabs[obj_hash].grid(row=4, column=0, rowspan=10, columnspan=2, padx=10, pady=(0, 5), sticky="news")
+        self.current_tab = self.info_tabs[obj_hash]
+        self.current_obj_hash = obj_hash
 
 
 def get_MainWindow():
